1b7c9722fe4aa8caf96c2a05b82c976498afd1d6
lizhenqi <1784913829@qq.com>
2021-03-09 22:47:46 +0800

    lizhenqi

--- 后台管理/houtai/src/views/Home.vue
+++ 后台管理/houtai/src/views/Home.vue
@@ -1,18 +1,131 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="home_top">
+      <div class="box1">
+        <span>电商后台管理系统</span>
+       
+      </div>
+      <div class="box2">
+        <button @click="logout" type="info">退出</button>
+      </div>
+    </div>
+    <div class="home_center">
+      <div class="home_left">
+        <div class="home_left_top">
+          <img src="../assets/图层1@2x.png" alt="">
+        </div>
+        <el-menu 
+          
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#333744"
+          text-color="#fff"
+          :router=true
+            :unique-opened=true
+        >
+          <el-submenu :index="item.id+'-1'+''" v-for="item in list" :key="item.id">
+            
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <el-menu-item-group>
+              <router-link :to="'/home/'+ite.path" :index="ite.id+'-6'+''"  v-for="ite in item.children" :key="ite.id" tag="el-menu-item">{{ite.authName}}</router-link>
+              
+            </el-menu-item-group>
+            
+
+          </el-submenu>
+
+         
+        </el-menu>
+      </div>
+
+      <div class="home_right">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      list:''
+    }
+  },
+  methods: {
+    logout(){
+      window.sessionStorage.clear();
+     this.$router.push('/login')
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+  created () {
+     this.axios.get('/api/menus').then(res=>{
+      console.log(res.data.data)
+      this.list = res.data.data
+    })
+    }
+  } 
+</script>
+
+<style lang="less" scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+
+.home_top {
+  width: 100%;
+  height: 60px;
+  background: #373d41;
+  display: flex;
+  justify-content: space-between;
+  .box1 {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    margin-left: 10px;
+    color: #ccc;
+  }
+  .box2 {
+    width: 100px;
+    display: flex;
+    align-items: center;
+    button {
+      padding: 10px 20px;
+    }
   }
 }
-</script>
+.home_center {
+  width: 100%;
+  height: 600px;
+  display: flex;
+  justify-content: space-between;
+  .home_left {
+    height: 100%;
+    background: #333744;
+    width: 200px;
+   
+  }
+  .home_right {
+    flex: 1;
+    background: #EAEDF1;
+  }
+}
+</style>
